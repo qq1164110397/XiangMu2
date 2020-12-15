7e650f2c88ec02cf3e2dc20e054baf915a39b9f7
liukexiangmu <1164110397@qq.com>
2020-12-15 15:30:32 +0800

    第一次提交

---
+++ app/src/main/java/com/example/xiangmu/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.xiangmu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+    }
+
+    private void initData() {
+
+        TextView tv = findViewById(R.id.tv);
+    }
+}
